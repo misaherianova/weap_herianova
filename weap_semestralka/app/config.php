d5e66899f35d8ce374d1ab70267578418dcf2b27
Michaela_Herianova <michaela.herianova@tul.cz>
2016-04-24 18:56:34 +0200

    pokus o propojeni databaze s hostingem
    + prodana nova slozka celku

--- weap_semestralka/app/config.php
+++ weap_semestralka/app/config.php
@@ -5,6 +5,15 @@ define('STATIC_DIR', BASE_DIR . '/static/');
 define('DEBUG', true);
 
 //databaze
+
+/*
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'misawe1461503257');
+define('DB_USER', 'misawe1461503257');
+define('DB_PASS', 'vDpZiZT');
+*/
+
 define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'database_eshop');
